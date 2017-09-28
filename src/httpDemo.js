f92b6cf14c1044a134241c4aaca7f5c1dc515a6d
summer <826288352@qq.com>
2017-09-28 17:23:41 +0800

    Update httpDemo.js

--- src/httpDemo.js
+++ src/httpDemo.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text-plain' });
     response.end('Hello World\n');
-}).listen(8124);
+}).listen(1234);
